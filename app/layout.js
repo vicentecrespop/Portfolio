8302547bb6a49b2c82797f2e9b4cb7efaab4823b
vicentecrespop <vicentecrespo45@gmail.com>
2023-07-04 11:49:22 -0300

    Added analytics

--- app/layout.js
+++ app/layout.js
@@ -2,6 +2,8 @@ import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+
+import { Analytics } from '@vercel/analytics/react';
  
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,12 +14,15 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="pt-BR">
-      <body className={inter.className}>
-        <Header />
-        {children}
-        <Footer />
-      </body>
-    </html>
+    <>
+      <html lang="pt-BR">
+        <body className={inter.className}>
+          <Header />
+          {children}
+          <Footer />
+        </body>
+      </html>
+      <Analytics />
+    </>
   )
 }
